452b92f84cd3254c8747a105dc5567e523eb9d1b
Art <artur@sneakpeeq.com>
2011-08-29 13:03:03 -0700

    adding js

---
+++ jquery.grayscale.js
@@ -0,0 +1,99 @@
+/*
+ * Grayscale plugin.
+ * @author arturnt
+ */
+(function($) {
+	
+	$.fn.grayscale = function() {
+	
+		function canvasToGray(imgObj, width, height) {
+			var canvas = document.createElement('canvas'),
+				ctx = canvas.getContext('2d');
+			
+			canvas.width = width;
+			canvas.height = height;
+			ctx.drawImage(imgObj, 0, 0);
+			
+			var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			
+			for ( var y = 0; y < imgPixels.height; y++ ) {
+				for ( var x = 0; x < imgPixels.width; x++ ) {
+					var i = (y * 4) * imgPixels.width + x * 4;
+					var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+					imgPixels.data[i] = avg;
+					imgPixels.data[i + 1] = avg;
+					imgPixels.data[i + 2] = avg;
+				}
+			}
+			
+			ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+			return canvas.toDataURL();
+		}
+		
+		function ieToGray(element) {
+	        $(element).css({
+	          'filter': 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)',
+	          'zoom': '1'
+	        });
+		}
+		
+		return $(this).each(function() {
+			
+			if(!!document.createElement('canvas').getContext) {
+				
+				var $this = $(this);
+				
+				if ( ! new RegExp(window.location.hostname).test($this.attr("src")) ) {
+					$.getImageData({
+			            url: $(this).attr('src'),
+			            success: function(image) {
+			            	$this.attr("src", canvasToGray(image, image.width, image.height));
+			            }
+			        });
+					
+				} else {
+					
+					var imgObj = new Image();
+					imgObj.src = $(this).attr("src");
+					$(this).attr("src", canvasToGray(imgObj, imgObj.width, imgObj.height));
+					
+				}
+				
+			} else if ($.browser.msie) { 
+				ieToGray(this);
+			}
+			
+		});
+		
+	}
+	
+})(jQuery);
+	
+
+/*
+*
+*  jQuery $.getImageData Plugin 0.3
+*  http://www.maxnov.com/getimagedata
+*  
+*  Written by Max Novakovic (http://www.maxnov.com/)
+*  Date: Thu Jan 13 2011
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*  
+*  Includes jQuery JSONP Core Plugin 2.1.4
+*  http://code.google.com/p/jquery-jsonp/
+*  Copyright 2010, Julian Aubourg
+*  Released under the MIT License.
+* 
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*  
+*  Copyright 2011, Max Novakovic
+*  Dual licensed under the MIT or GPL Version 2 licenses.
+*  http://www.maxnov.com/getimagedata/#license
+* 
+*/
+(function(c,g){function n(){}function o(a){s=[a]}function e(a,j,k){return a&&a.apply(j.context||j,k)}function h(a){function j(b){!l++&&g(function(){p();q&&(t[d]={s:[b]});z&&(b=z.apply(a,[b]));e(a.success,a,[b,A]);e(B,a,[a,A])},0)}function k(b){!l++&&g(function(){p();q&&b!=C&&(t[d]=b);e(a.error,a,[a,b]);e(B,a,[a,b])},0)}a=c.extend({},D,a);var B=a.complete,z=a.dataFilter,E=a.callbackParameter,F=a.callback,R=a.cache,q=a.pageCache,G=a.charset,d=a.url,f=a.data,H=a.timeout,r,l=0,p=n;a.abort=function(){!l++&&
+p()};if(e(a.beforeSend,a,[a])===false||l)return a;d=d||u;f=f?typeof f=="string"?f:c.param(f,a.traditional):u;d+=f?(/\?/.test(d)?"&":"?")+f:u;E&&(d+=(/\?/.test(d)?"&":"?")+encodeURIComponent(E)+"=?");!R&&!q&&(d+=(/\?/.test(d)?"&":"?")+"_"+(new Date).getTime()+"=");d=d.replace(/=\?(&|$)/,"="+F+"$1");q&&(r=t[d])?r.s?j(r.s[0]):k(r):g(function(b,m,v){if(!l){v=H>0&&g(function(){k(C)},H);p=function(){v&&clearTimeout(v);b[I]=b[w]=b[J]=b[x]=null;i[K](b);m&&i[K](m)};window[F]=o;b=c(L)[0];b.id=M+S++;if(G)b[T]=
+G;var O=function(y){(b[w]||n)();y=s;s=undefined;y?j(y[0]):k(N)};if(P.msie){b.event=w;b.htmlFor=b.id;b[I]=function(){/loaded|complete/.test(b.readyState)&&O()}}else{b[x]=b[J]=O;P.opera?(m=c(L)[0]).text="jQuery('#"+b.id+"')[0]."+x+"()":b[Q]=Q}b.src=d;i.insertBefore(b,i.firstChild);m&&i.insertBefore(m,i.firstChild)}},0);return a}var Q="async",T="charset",u="",N="error",M="_jqjsp",w="onclick",x="on"+N,J="onload",I="onreadystatechange",K="removeChild",L="<script/>",A="success",C="timeout",P=c.browser,
+i=c("head")[0]||document.documentElement,t={},S=0,s,D={callback:M,url:location.href};h.setup=function(a){c.extend(D,a)};c.jsonp=h})(jQuery,setTimeout);
+(function(c){c.getImageData=function(a){var f=/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;if(a.url){var g=location.protocol==="https:",e="";e=a.server&&f.test(a.server)&&a.server.indexOf("https:")&&(g||a.url.indexOf("https:"))?a.server:"//img-to-json.appspot.com/?callback=?";c.jsonp({url:e,data:{url:escape(a.url)},dataType:"jsonp",timeout:1E4,success:function(b){var d=new Image;c(d).load(function(){this.width=b.width;this.height=b.height;typeof a.success==typeof Function&& a.success(this)}).attr("src",b.data)},error:function(b,d){typeof a.error==typeof Function&&a.error(b,d)}})}else typeof a.error==typeof Function&&a.error(null,"no_url")}})(jQuery);
